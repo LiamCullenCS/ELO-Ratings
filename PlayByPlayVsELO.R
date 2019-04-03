ef03228191e82555bf19d4cfa461a16f82ea3842
Liam Cullen <LiamCullenCS@gmail.com>
2019-04-03 16:25:05 +0100

    Seahawks Pie Chart vs League Averages

--- PlayByPlayVsELO.R
+++ PlayByPlayVsELO.R
@@ -18,12 +18,39 @@ seaDF <- as.data.frame(format(table(seaDat)))
 
 plot(factor(seaDat), main = "Hawks Play Distribution 2018")
 
-allDat <- new_dat$PlayType[new_dat$PlayType == 'Pass'
-	& new_dat$PlayType == 'Run'
-	& new_dat$PlayType != 'Sack']
+allDat <- new_dat$PlayType[new_dat$PlayType != 'Spike'
+	& new_dat$PlayType != 'Extra Point'
+	& new_dat$PlayType != 'No Play'
+	& new_dat$PlayType != 'Kickoff'
+	& new_dat$PlayType != 'Field Goal'
+	& new_dat$PlayType != 'Punt'
+	& new_dat$PlayType != 'End of Game'
+	& new_dat$PlayType != 'QB Kneel'
+	& new_dat$PlayType != 'Two Minute Warning'
+	& new_dat$PlayType != 'Quarter End'
+	& new_dat$PlayType != 'Timeout'
+	& new_dat$PlayType != 'Half End'
+	]
 
-
+library(wesanderson)
 par(mfrow = c(2,1))
-pie(table(seaDat), main = "Seahawks Play Distribution")
 
-pie(table(allDat), main = "League Average Play Distribution")
+sea_cols <- c(nflteams$primary[nflteams$abbr == 'SEA'], 
+	nflteams$secondary[nflteams$abbr == 'SEA'], 
+	nflteams$tertiary[nflteams$abbr == 'SEA'])
+
+A <- table(seaDat)
+B <- table(allDat)
+
+Apercentlabels <- round(100*A/sum(A), 2)
+Bpercentlabels <- round(100*B/sum(B), 2)
+
+apielabel <- paste(Apercentlabels, "%", sep = "")
+bpielabel <- paste(Bpercentlabels, "%", sep = "")
+
+pie(table(seaDat), labels = c(apielabel, table(seaDat)), main = "Seahawks Play Distribution", col = sea_cols)
+legend("topright", names(table(seaDat)), cex = 0.8, fill = sea_cols)
+pie(table(allDat), labels = c(bpielabel, table(allDat)), main = "League Average Play Distribution", col = wes_palette("Zissou1"))
+legend("topright", names(table(allDat)), cex = 0.8, fill = wes_palette("Zissou1"))
+
+
