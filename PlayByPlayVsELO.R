4b55cbc9bcaf0fee58631d4418eead066ad01abe
Liam Cullen <LiamCullenCS@gmail.com>
2019-04-04 18:02:23 +0100

    Team Play Distributions & Percent Difference from League Average

--- PlayByPlayVsELO.R
+++ PlayByPlayVsELO.R
@@ -1,13 +1,10 @@
 library(nflscrapR)
+library(wesanderson)
 
 new_dat <- season_play_by_play(2018)
 
 #Trim dataframe to show only Passes(plus sacks) and Runs
-
-seaDF <- as.data.frame(format(table(seaDat)))
-
-plot(factor(seaDat), main = "Hawks Play Distribution 2018")
-
+#Get League Wide Averages:
 allDat <- new_dat$PlayType[new_dat$PlayType != 'Spike'
 	& new_dat$PlayType != 'Extra Point'
 	& new_dat$PlayType != 'No Play'
@@ -19,21 +16,17 @@ allDat <- new_dat$PlayType[new_dat$PlayType != 'Spike'
 	& new_dat$PlayType != 'Two Minute Warning'
 	& new_dat$PlayType != 'Quarter End'
 	& new_dat$PlayType != 'Timeout'
-	& new_dat$PlayType != 'Half End'
-	]
+	& new_dat$PlayType != 'Half End']
 
-library(wesanderson)
-
-B <- table(allDat)
-Bpercentlabels <- round(100*B/sum(B), 2)
-
-bpielabel <- paste(Bpercentlabels, "%", sep = "")
-pie(table(allDat), labels = c(bpielabel, table(allDat)), main = "League Average Play Distribution", col = wes_palette("Zissou1"))
+leagueAvgs <- table(allDat)
+leaguePercents <- round(100*B/sum(leagueAvgs), 2)
+leagueBreakdown <- paste(leaguePercents, "%", sep = "")
+pie(table(allDat), labels = c(leagueBreakdown, table(allDat)), main = "League Average Play Distribution", col = wes_palette("Zissou1"))
 legend("topright", names(table(allDat)), cex = 0.8, fill = wes_palette("Zissou1"))
 
 
-
-playDist <- new_dat$PlayType[new_dat$posteam == 'CHI' 
+#Get Team Specific Play Distribution:
+playDist <- new_dat$PlayType[new_dat$posteam == 'ATL'
 	& new_dat$PlayType != 'Spike'
 	& new_dat$PlayType != 'Extra Point'
 	& new_dat$PlayType != 'No Play'
@@ -44,13 +37,57 @@ playDist <- new_dat$PlayType[new_dat$posteam == 'CHI'
 	& new_dat$PlayType != 'QB Kneel'
 	& new_dat$PlayType != 'Timeout']
 
-team_cols <- c(nflteams$primary[nflteams$abbr == 'CHI'], 
-	nflteams$secondary[nflteams$abbr == 'CHI'], 
-	nflteams$tertiary[nflteams$abbr == 'CHI'])
+team_cols <- c(nflteams$primary[nflteams$abbr == 'ATL'], 
+		nflteams$secondary[nflteams$abbr == 'ATL'], 
+		nflteams$tertiary[nflteams$abbr == 'ATL'])
 
 A <- table(playDist)
 Apercentlabels <- round(100*A/sum(A), 2)
 apielabel <- paste(Apercentlabels, "%", sep = "")
+pie(table(playDist), labels = c(apielabel, table(playDist)), main = "Falcons Play Distribution", col = team_cols)
+legend("topright", names(table(playDist)), cex = 0.8, fill = team_cols) 
 
-pie(table(playDist), labels = c(apielabel, table(playDist)), main = "Bears Play Distribution", col = team_cols)
-legend("topright", names(table(playDist)), cex = 0.8, fill = team_cols)
+
+#########################################################################
+# Build Table of Results
+#########################################################################
+
+
+Pass <- 0
+Run <- 0
+Sack <- 0
+PassDiff <- 0
+RunDiff <- 0
+SackDiff <- 0 
+team <- nflteams$abbr[nflteams$abbr != 'SD' 
+			& nflteams$abbr != 'STL' 
+			& nflteams$abbr != 'JAC']
+
+TeamDistributions <- data.frame(team, Pass, Run, Sack, PassDiff, RunDiff, SackDiff)
+
+for (i in 1:32){
+	playDist <- new_dat$PlayType[new_dat$posteam == team[i]
+	& new_dat$PlayType != 'Spike'
+	& new_dat$PlayType != 'Extra Point'
+	& new_dat$PlayType != 'No Play'
+	& new_dat$PlayType != 'Kickoff'
+	& new_dat$PlayType != 'Field Goal'
+	& new_dat$PlayType != 'Punt'
+	& new_dat$PlayType != 'End of Game'
+	& new_dat$PlayType != 'QB Kneel'
+	& new_dat$PlayType != 'Timeout']
+
+	A <- table(playDist)
+	percents <- round(100*A/sum(A), 2)
+	playBreakdown <- paste(percents, "%", sep = "")
+	
+	TeamDistributions$Pass[TeamDistributions$team == team[i]] <- playBreakdown[1]
+	TeamDistributions$Run[TeamDistributions$team == team[i]] <- playBreakdown[2]
+	TeamDistributions$Sack[TeamDistributions$team == team[i]] <- playBreakdown[3]
+	TeamDistributions$PassDiff[TeamDistributions$team == team[i]] <- (as.numeric(sub("%", "", playBreakdown[1])) - as.numeric(sub("%", "", leagueBreakdown[1])))
+	TeamDistributions$RunDiff[TeamDistributions$team == team[i]] <- (as.numeric(sub("%", "", playBreakdown[2])) - as.numeric(sub("%", "", leagueBreakdown[2])))
+	TeamDistributions$SackDiff[TeamDistributions$team == team[i]] <- (as.numeric(sub("%", "", playBreakdown[3])) - as.numeric(sub("%", "", leagueBreakdown[3])))
+}
+
+View(TeamDistributions)
+	
